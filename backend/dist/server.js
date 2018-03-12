303374662ce7116809c877ac8a3a369e1d90f249
Ilya <Ilya@Ilya-Sosnovsky.local>
2018-03-12 23:24:44 +0400

    fix: some fixes

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -11,6 +11,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
+const passport = require("koa-passport");
+const session = require("koa-session");
+const VKontakteStrategy = require("passport-vkontakte").Strategy;
 const db_1 = require("./db");
 const config_1 = require("./config");
 const AuthController_1 = require("./controllers/AuthController");
@@ -20,53 +23,48 @@ const AuthRepository = new AuthRepository_1.default();
 const AuthService = new AuthService_1.default(AuthRepository);
 const AuthController = new AuthController_1.default(AuthService);
 const app = new Koa();
+const router = new Router({
+    prefix: "/api"
+});
 db_1.default.sync({ force: true });
-app.use(bodyParser());
-const router = new Router();
-app.use(router.routes());
 router.post("/join", (ctx) => __awaiter(this, void 0, void 0, function* () {
-    console.log("sdf");
     AuthController.join(ctx);
 }));
-app.listen(config_1.default.port, () => {
-    console.log(`Server running on ${config_1.default.port}`);
-});
-const VKontakteStrategy = require("passport-vkontakte").Strategy;
-// User session support middlewares. Your exact suite might vary depending on your app's needs.
-app.use(require("cookie-parser")());
-app.use(require("body-parser").urlencoded({ extended: true }));
-app.use(require("express-session")({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+app.keys = ["secret"];
+app
+    .use(bodyParser())
+    .use(router.routes())
+    .use(session({}, app))
+    .use(passport.initialize())
+    .use(passport.session());
 passport.use(new VKontakteStrategy({
-    clientID: VKONTAKTE_APP_ID,
-    clientSecret: VKONTAKTE_APP_SECRET,
+    clientID: "6405800",
+    clientSecret: "JL1BXqyTkRpDRnwbd7oJ",
     callbackURL: "http://localhost:3000/auth/vkontakte/callback"
 }, function myVerifyCallbackFn(accessToken, refreshToken, params, profile, done) {
-    // Now that we have user's `profile` as seen by VK, we can
-    // use it to find corresponding database records on our side.
-    // Also we have user's `params` that contains email address (if set in
-    // scope), token lifetime, etc.
-    // Here, we have a hypothetical `User` class which does what it says.
-    User.findOrCreate({ vkontakteId: profile.id })
-        .then(function (user) {
-        done(null, user);
-    })
-        .catch(done);
+    // // Now that we have user's `profile` as seen by VK, we can
+    // // use it to find corresponding database records on our side.
+    // // Also we have user's `params` that contains email address (if set in
+    // // scope), token lifetime, etc.
+    // // Here, we have a hypothetical `User` class which does what it says.
+    // User.findOrCreate({ vkontakteId: profile.id })
+    // 	.then(function(user) {
+    // 		done(null, user);
+    // 	})
+    // 	.catch(done);
 }));
 // User session support for our hypothetical `user` objects.
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-passport.deserializeUser(function (id, done) {
-    User.findById(id)
-        .then(function (user) {
-        done(null, user);
-    })
-        .catch(done);
+// passport.serializeUser(function(user, done) {
+// 	done(null, user.id);
+// });
+// passport.deserializeUser(function(id, done) {
+// 	User.findById(id)
+// 		.then(function(user) {
+// 			done(null, user);
+// 		})
+// 		.catch(done);
+// });
+app.listen(config_1.default.port, () => {
+    console.log(`Server running on ${config_1.default.port}`);
 });
 //# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as bodyParser from "koa-bodyparser";
-import * as passport from "passport";
+import * as passport from "koa-passport";
+import * as session from "koa-session";
+const VKontakteStrategy = require("passport-vkontakte").Strategy;
 
 import sequelize from "./db";
 import config from "./config";
@@ -10,27 +12,29 @@ import AuthControllerFactory from "./controllers/AuthController";
 import AuthServiceFactory from "./services/AuthService";
 import AuthRepositoryFactory from "./repositories/AuthRepository";
 
-import { VKontakteStrategy } from "passport-vkontakte";
-
 const AuthRepository = new AuthRepositoryFactory();
 const AuthService = new AuthServiceFactory(AuthRepository);
 const AuthController = new AuthControllerFactory(AuthService);
 
 const app = new Koa();
+const router = new Router({
+	prefix: "/api"
+});
 
 sequelize.sync({ force: true });
-
-app.use(bodyParser());
-
-const router = new Router();
-app.use(router.routes());
 
 router.post("/join", async ctx => {
 	AuthController.join(ctx);
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.keys = ["secret"];
+
+app
+	.use(bodyParser())
+	.use(router.routes())
+	.use(session({}, app))
+	.use(passport.initialize())
+	.use(passport.session());
 
 passport.use(
 	new VKontakteStrategy(
@@ -46,32 +50,32 @@ passport.use(
 			profile: string,
 			done: () => void
 		) {
-			// Now that we have user's `profile` as seen by VK, we can
-			// use it to find corresponding database records on our side.
-			// Also we have user's `params` that contains email address (if set in
-			// scope), token lifetime, etc.
-			// Here, we have a hypothetical `User` class which does what it says.
-			User.findOrCreate({ vkontakteId: profile.id })
-				.then(function(user) {
-					done(null, user);
-				})
-				.catch(done);
+			// // Now that we have user's `profile` as seen by VK, we can
+			// // use it to find corresponding database records on our side.
+			// // Also we have user's `params` that contains email address (if set in
+			// // scope), token lifetime, etc.
+			// // Here, we have a hypothetical `User` class which does what it says.
+			// User.findOrCreate({ vkontakteId: profile.id })
+			// 	.then(function(user) {
+			// 		done(null, user);
+			// 	})
+			// 	.catch(done);
 		}
 	)
 );
 
 // User session support for our hypothetical `user` objects.
-passport.serializeUser(function(user, done) {
-	done(null, user.id);
-});
+// passport.serializeUser(function(user, done) {
+// 	done(null, user.id);
+// });
 
-passport.deserializeUser(function(id, done) {
-	User.findById(id)
-		.then(function(user) {
-			done(null, user);
-		})
-		.catch(done);
-});
+// passport.deserializeUser(function(id, done) {
+// 	User.findById(id)
+// 		.then(function(user) {
+// 			done(null, user);
+// 		})
+// 		.catch(done);
+// });
 
 app.listen(config.port, () => {
 	console.log(`Server running on ${config.port}`);
