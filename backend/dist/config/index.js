35185a5c579444fb4ca57656a3ade543b674cff0
Сосновский Илья <isosnovskii@htc-cs.ru>
2018-03-19 11:00:16 +0400

    'remove

--- backend/dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    database: "tic-tac-toe",
-    dialect: "postgres",
-    port: 4000,
-    name: "Ilya"
-};
-//# sourceMappingURL=index.js.map
