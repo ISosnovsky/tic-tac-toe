50949dbfe2232da816acd1960c4f2c0b49c82824
Ilya Sosnovsky <highfive.bear@gmail.com>
2018-03-22 23:50:02 +0400

    feat: add DI container

--- backend/src/lib/serviceLocator.ts
+++ backend/src/lib/serviceLocator.ts
@@ -1,22 +1,25 @@
+const fnArgs = require("fn-args");
+
 module.exports = function() {
-	const dependencies: { name?: string } = {};
-	const factories: { name?: string } = {};
-	const serviceLocator: {
-		factory?: (name: string, factory: object) => void;
+	const dependencies: { [name: string]: object } = {};
+	const factories: { [name: string]: () => void } = {};
+	const diContainer: {
+		factory?: (name: string, factory: () => void) => void;
 		register?: (name: string, instance: object) => void;
-		get?: (name: string) => dependencies;
+		get?: (name: string) => object;
+		inject?: (factory: () => void) => object;
 	} = {};
 
-	serviceLocator.factory = function(name: string, factory: object) {
+	diContainer.factory = (name: string, factory: () => void) => {
 		factories[name] = factory;
 	};
-	serviceLocator.register = function(name: string, instance: object) {
+	diContainer.register = (name: string, instance: object) => {
 		dependencies[name] = instance;
 	};
-	serviceLocator.get = function(name: string) {
+	diContainer.get = (name: string) => {
 		if (!dependencies[name]) {
-			const factory = factories[name];
-			dependencies[name] = factory && factory(serviceLocator);
+			const factory: () => void = factories[name];
+			dependencies[name] = factory && diContainer.inject(factory);
 
 			if (!dependencies[name]) {
 				throw new Error("Cannot find module: " + name);
@@ -24,5 +27,12 @@ module.exports = function() {
 		}
 		return dependencies[name];
 	};
-	return serviceLocator;
+	diContainer.inject = factory => {
+		const args = fnArgs(factory).map((dependency: any) =>
+			diContainer.get(dependency)
+		);
+
+		return factory.apply(null, args);
+	};
+	return diContainer;
 };
